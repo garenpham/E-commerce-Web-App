4a8ca1f74ce5c76bef06dbe5fb911e51d13c9422
garenpham <tanphamminh2002@gmail.com>
2023-09-17 00:46:26 -0700

    settings name

---
+++ admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,17 @@
+import prismadb from '@/lib/prismadb'
+import React from 'react'
+
+type Props = {
+  params: { storeId: string }
+}
+
+const DashboardPage = async ({ params }: Props) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  })
+  return <div>Active store: {store?.name}</div>
+}
+
+export default DashboardPage
