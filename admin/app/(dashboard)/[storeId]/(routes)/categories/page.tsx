8ce09c02ddf8d6da3c72dcd1ef72e340b4ce9615
garenpham <tanphamminh2002@gmail.com>
2023-09-20 18:39:59 -0700

    categories admin route

---
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,42 @@
+import prismadb from '@/lib/prismadb'
+import { format } from 'date-fns'
+import React from 'react'
+import CategoryClient from './components/client'
+import { CategoryColumn } from './components/columns'
+
+type Props = {
+  params: {
+    storeId: string
+  }
+}
+
+async function page({ params }: Props) {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className={`flex-1 space-y-4 p-8 pt-6`}>
+        <CategoryClient data={formattedCategories} />
+      </div>
+    </div>
+  )
+}
+
+export default page
